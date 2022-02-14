52af47d719c5baa9f9268c2a771e6bf4f2ad968b
Benny Iko <benny.iko.it@gmail.com>
2022-02-13 20:52:09 -0500

    Changes to be committed: 	modified:   Main.java     On branch master 	Changes: 	verified functionality of Aspects 	verified add(String), delete(String), update(String) functionality of Contacts 	Issues: 	N/A

--- Main.java
+++ Main.java
@@ -3,7 +3,11 @@ package aop;
 public class Main {
 
 	public static void main(String[] args) {
-		
+		Contact sample = new Contact ("John", "123 Fake St.", "Fakeville", "Fakeland", "12345", "123-456-7890");
+		Contacts book = new Contacts();
+		book.add(sample);
+		book.update("John");
+		book.delete("Hewey");
 	}
 
 }
