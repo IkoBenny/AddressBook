6b105a31660880dc5f54862594ec23655b1c048d
Benny Iko <benny.iko.it@gmail.com>
2022-02-20 08:22:14 -0500

    Changes to be committed: 	modified:   Contact.java     On branch master
    	Changes:
    	implemented clone()
    	Issues:
    	N/A

--- Contact.java
+++ Contact.java
@@ -1,6 +1,6 @@
 package aop;
 
-public class Contact {
+public class Contact implements Cloneable {
 	String name;
 	String street;
 	String city;
@@ -15,6 +15,11 @@ public class Contact {
 		this.state = state;
 		this.zip = zip;
 		this.phone = phone;
+	}
+	
+	public Object clone () {
+		Object o = new Contact(this.name, this.street, this.city, this.state, this.zip, this.phone);
+		return o;
 	}
 	
 	public String toString() {
@@ -33,12 +38,15 @@ public class Contact {
 	
 	public static void main(String [] args) {
 		Contact c = new Contact("Mr. Fake", "123 Fake St", "Fakeville", "FA", "12345", "123-456-7890");
-		Contact d = new Contact("Mr. Fake", "123 Fake St", "Fakeville", "FA", "12345", "123-456-7890");
+		Contact d = new Contact("Mr. Fake", "123 Fakdfse St", "Fakdfsseville", "FAsfd", "12dfs345", "123-456-7sfs890");
+		c = (Contact)d.clone();
 		Contacts book = new Contacts();
 		book.add(c);
 		book.add(d);
-		book.delete("Mr. Fake");
+		//book.delete("Mr. Fake");
 		book.display();
 	}
+	
+	
 
 }
