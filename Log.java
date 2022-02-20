ebd20ed11510aa63e2c056474d9cd88590eeed72
Benny Iko <benny.iko.it@gmail.com>
2022-02-19 21:32:59 -0500

    Changes to be committed: 	modified:   Log.java     On branch master 	Changes: 	Added writeUpdate() and writeDelete() methods for writing to file 	Issues: 	N/A

--- Log.java
+++ Log.java
@@ -8,15 +8,27 @@ import java.io.IOException;
 public class Log {
 	File obj;
 	FileWriter writer;
+	Contact c;
 	
-	public Log() {
+	public Log(Contact c) {
 		obj = new File("log.txt");
+		this.c = c;
 	}
 	
-	public void write() {
+	public void writeUpdate() {
 		try {
 			writer = new FileWriter(obj, true);
-			writer.write("File Write Operation\n");
+			writer.write("Record Updated:" + c.toString() + "\n");
+		    writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}	
+	}
+	
+	public void writeDelete() {
+		try {
+			writer = new FileWriter(obj, true);
+			writer.write("Record Deleted:" + c.toString() + "\n");
 		    writer.close();
 		} catch (IOException e) {
 			e.printStackTrace();
