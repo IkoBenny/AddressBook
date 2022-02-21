826713b5edc62714867325d767ddbe765be02948
Benny Iko <benny.iko.it@gmail.com>
2022-02-20 23:03:12 -0500

    Changes to be committed: 	modified:   Contacts.java     On branch master 	Changes: 	delete() and update() method implementations 	Issues: 	N/A

--- Contacts.java
+++ Contacts.java
@@ -18,23 +18,34 @@ public class Contacts {
 		Contact c = create();
 		contacts.add(c);
 	}
-
-	public Contact delete(String name) {
-		Contact c = null;
-		for (int i = 0; i < contacts.size(); i++) {
-			if (name.equals(contacts.get(i).getName())) {
-				c = contacts.get(i);
-				contacts.remove(i);
-			}
+	
+	public int getIndex(String name) {
+		int index = 99;
+		for(int i = 0; i < contacts.size(); i++ ) {
+			if (contacts.get(i).getName().equals(name))
+				index = i;			
 		}
-		return c;
+		return index;
 	}
 
+	public Contact delete() {
+		Contact c = null;
+		String name;
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter contact's full name: ");
+		name = in.nextLine();
+		int index = getIndex(name);
+		c = get(name);
+		contacts.remove(index);
+		return c;		
+	}
+
+	
 	public Contact get(String name) {
 		Contact contact = null;
-		for (Contact c : contacts) {
-			if (c.getName().equals(name))
-				contact = c;
+		for (int i =0; i < contacts.size(); i ++) {
+			if (contacts.get(i).getName().equals(name))
+				contact = contacts.get(i);
 		}
 		return contact;
 	}
@@ -43,7 +54,7 @@ public class Contacts {
 		Contact c;
 		Scanner in = new Scanner(System.in);
 		String name, street, city, state, zip, phone;
-		System.out.println("Enter contact name: ");
+		System.out.println("Enter contact's full name: ");
 		name = in.nextLine();
 		System.out.println("Enter street name: ");
 		street = in.nextLine();
@@ -55,16 +66,18 @@ public class Contacts {
 		zip = in.nextLine();
 		System.out.println("Enter phone number: ");
 		phone = in.nextLine();
-		in.close();
+		//in.close();
 		c = new Contact(name, street, city, state, zip, phone);
 		return c;
 	}
 	
-	public Contact update(String name) {
-		Contact c = get(name);
-		Contact update = create();
-		add(update);
-		return c;
+	public Contact update() {
+		Contact c = create();
+		String name = c.getName();
+		Contact old = get(name);
+		int index = getIndex(name);
+		contacts.set(index, c); 
+		return old;
 	}
 
 	public void display() {
@@ -73,4 +86,15 @@ public class Contacts {
 		}
 	}
 
+	public static void main(String [] args) {
+		Contacts book = new Contacts();
+		Contact sample = new Contact ("John", "123 Fake St.", "Fakeville", "Fakeland", "12345", "123-456-7890");
+		Contact sample2 = new Contact ("Johngdfg", "123 Fake St.", "Fakeville", "Fakeland", "12345", "123-456-7890");
+		Contact sample3 = new Contact ("Jdfgdohn", "123 Fake St.", "Fakeville", "Fakeland", "12345", "123-456-7890");
+		book.add(sample);
+		book.add(sample2);
+		book.add(sample3);
+		System.out.println(book.getIndex("Jdfgdohn"));
+		
+	}
 }
